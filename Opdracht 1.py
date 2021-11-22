af88133345dd8aac1e230fbb7a2f17b9a0fdba82
Mike Spaans <99069233@mydavinci.nl>
2021-11-22 13:22:27 +0100

    Opdracht 1

---
+++ Opdracht 1.py
@@ -0,0 +1,11 @@
+DieselPrijs = input("Wat is de dieselprijs? ")
+AantalPersonen = 4
+Afstand = 244
+PrijsBroodKoffie = 8
+
+TotaalBroodKoffie = PrijsBroodKoffie * AantalPersonen
+DieselVerbruik = Afstand / 12
+KostenDiesel = float(DieselPrijs) * DieselVerbruik
+TotaalKostenHeen = KostenDiesel + TotaalBroodKoffie
+TotaalHeenTerug = TotaalKostenHeen * 2
+print (TotaalHeenTerug)
