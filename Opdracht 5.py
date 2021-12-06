acb6984ab987b37de750be09fd23cda2d23be84a
Mike Spaans <99069233@mydavinci.nl>
2021-12-06 13:19:55 +0100

    Opdracht 5

--- Opdracht 5.py
+++ Opdracht 5.py
@@ -6,19 +6,37 @@ VraagFlexibel = int(input("Op een schaal van 1 tot 10 hoe flexibel bent u? (1 is
 VraagPositief = int(input("Op een schaal van 1 tot 10 hoe positief bent u? (1 is totaal niet en 10 heel erg) "))
 VraagDoorzettend = int(input("Op een schaal van 1 tot 10 hoe doorzettend bent u? (1 is totaal niet en 10 heel erg) "))
 
+VraagKracht = 1
+VraagSpijkerBuigen = 1
+VraagIQ = 1
+VraagKastInElkaar = 301
+VraagSlotenSpringen = 1
+VraagVuurMaken = 70
+VraagEetbarePaddeStoelen = 1
+VraagGiftigeKruiden = 1
+VraagGeldBijleggen = 1
+
 if VraagLeeftijd > 16 and VraagFlexibel > 5 and VraagPositief > 5 and VraagDoorzettend > 5:
     # De Beer
-    VraagKracht = int(input("Hoeveel KG kunt u drukken? "))
-    VraagSpijkerBuigen = int(input("wat is de dikste spijker die u kunt buigen in mm? "))
+    while VraagKracht < 100:
+        VraagKracht = int(input("Hoeveel KG kunt u drukken? "))
+    while VraagSpijkerBuigen <10:
+        VraagSpijkerBuigen = int(input("wat is de dikste spijker die u kunt buigen in mm? "))
     # De Vos
-    VraagIQ = int(input("Wat is uw IQ? "))
-    VraagKastInElkaar = int(input("Binnen hoeveel seconden kunt u een IKEA Larsfrid kast in elkaar zetten? "))
+    while VraagIQ < 129:
+        VraagIQ = int(input("Wat is uw IQ? "))
+    while VraagKastInElkaar > 300:
+        VraagKastInElkaar = int(input("Binnen hoeveel seconden kunt u een IKEA Larsfrid kast in elkaar zetten? "))
     # De Bever
-    VraagSlotenSpringen = int(input("Wat is de grootste sloot waar u over heen kan springen in meters? "))
-    VraagVuurMaken = int(input("Binnen hoeveel seconden kunt u vuur maken met vuurstenen en hooi? "))
+    while VraagSlotenSpringen < 2:
+        VraagSlotenSpringen = int(input("Wat is de grootste sloot waar u over heen kan springen in meters? "))
+    while VraagVuurMaken > 61:
+        VraagVuurMaken = int(input("Binnen hoeveel seconden kunt u vuur maken met vuurstenen en hooi? "))
     # De Uil
-    VraagEetbarePaddeStoelen = int(input("Hoeveel eetbare paddestoelen kunt u herkennen?"))
-    VraagGiftigeKruiden = int(input("Hoeveel giftige kruiden kunt u herkennen"))
+    while VraagEetbarePaddeStoelen < 9:
+        VraagEetbarePaddeStoelen = int(input("Hoeveel eetbare paddestoelen kunt u herkennen? "))
+    while VraagGiftigeKruiden < 19:
+        VraagGiftigeKruiden = int(input("Hoeveel giftige kruiden kunt u herkennen? "))
 
     VraagGeldBijleggen = float(input("Hoeveel geld bent u bereid bij te leggen voor dit avontuur? "))
 
